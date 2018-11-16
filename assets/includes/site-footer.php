654e9d645042276c3e6bc5d7fdd981c30e4530f8
Aaron Greene <aarongreenedesign@gmail.com>
2018-11-16 07:06:22 -0500

    Testing PHP within Netlify

---
+++ assets/includes/site-footer.php
@@ -0,0 +1,10 @@
+<div class="site-footer">
+  <div class="footer-social">
+    Follow Me on Instagram -
+    <a href="https://www.instagram.com/aarons_sketchbook" class="social social-instagram">My Art</a>
+    <a href="https://www.instagram.com/aaron0574" class="social social-instagram">My Life</a>
+  </div>
+  <div class="copyright">
+    <p>Copyright &copy; Aaron's Sketchbook</p>
+  </div>
+</div>
--- test.php
+++ test.php
@@ -110,16 +110,7 @@
 		<!-- End Content -->
 
 		<!-- Footer -->
-		<div class="site-footer">
-			<div class="footer-social">
-				Follow Me on Instagram -
-				<a href="https://www.instagram.com/aarons_sketchbook" class="social social-instagram">My Art</a>
-				<a href="https://www.instagram.com/aaron0574" class="social social-instagram">My Life</a>
-			</div>
-			<div class="copyright">
-				<p>Copyright &copy; Aaron's Sketchbook</p>
-			</div>
-		</div>
+		<?php include '/assets/includes/site-footer.php'; ?>
 		<!-- End Footer -->
 	</div>
 
